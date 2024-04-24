0db8ac54bfa7ab9db46bd616e278b1ffcdb1a06a
kefirr85 <166715341+kefirr85@users.noreply.github.com>
2024-04-24 20:31:19 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,14 @@
+my_list = ['apple', 'banana', 'orange', 'kiwi', 'mango']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:4])
+my_list[2] = 'pizza'
+print(my_list)
+
+my_dict = {'help' : 'помощ', 'mom' : 'мама'}
+print(my_dict)
+print(my_dict.get('help'))
+my_dict['help'] = 'помогите'
+print(my_dict)
+my_dict['go'] = 'иди'
+print(my_dict)
